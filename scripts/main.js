a6ae93863bbd2ff0bb1713b97dd78668ab7b04af
hls93 <hlshaw3@ncsu.edu>
2017-07-13 14:42:19 -0400

    adding more notes

--- scripts/main.js
+++ scripts/main.js
@@ -19,13 +19,17 @@ let total = 0
      if (hand[i] === "2" || hand[i] === "3" || hand[i] === "4" || hand[i] === "5" || hand[i] === "6" || hand[i] === "7" || hand[i] === "8" || hand[i] === "9" || hand[i] === "10"); {
 //parseInt will parse through a string and return an integer
      total += parseInt(hand[i]);
+//If we don't get an integer, our other option is to get a head card with a value of 10
    } else if (hand[i] == "J" || hand[i] == "Q" || hand[i] == "K") {
      total += 10;
    }
 }
+//The Ace has to options so we need another for loop
  for (i = 0; i < hand.length; i++) {
+//If total is less than 11, ace of 11 will get us closer to 21
      if (hand[i] == "A" && total < 11) {
-     total += 11;          total
+     total += 11;
+//if total is greater than 11 then 1 will not push us over 21        
    } else if (hand[i] == "A" && total > 11) {
      total += 1;
    }
